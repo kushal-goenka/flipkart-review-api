c0731e47cb0bf8079071be4696df2b82cd146b60
Kushal-Goenka <kushalgoenka97@gmail.com>
2020-10-12 12:40:37 -0500

    adding command line arguments

--- reviewapi/spiders/reviewSpider.py
+++ reviewapi/spiders/reviewSpider.py
@@ -3,25 +3,17 @@ from scrapy.selector import Selector
 
 class reviewSpider(scrapy.Spider):
     name = "review"
-   
-    # def start_requests(self):
-        # url = "https://www.flipkart.com/wizo-dz09-phone-smartwatch/product-reviews/itmcc3b4c2842176?pid=SMWF8SY3R7YTUGBX&lid=LSTSMWF8SY3R7YTUGBX2UMRHQ&marketplace=FLIPKART"
 
-        # url = "https://www.flipkart.com/realme-buds-air-neo-bluetooth-headset/product-reviews/itmb11aa4151bfb6?pid=ACCFRSARNYQ4CNZG&lid=LSTACCFRSARNYQ4CNZGCQVAZI&marketplace=FLIPKART"
-
-        # url = 
-
-
-    
-        # yield scrapy.Request(url=url, callback=self.parse)
-
-    start_urls = ["https://www.flipkart.com/1-art-creations-vastu-seven-running-horses-uv-textured-framed-digital-reprint-14-inch-x-20-painting/p/itmf79hzphvfk7rr?pid=PTGF79FGDBBSFGRF&lid=LSTPTGF79FGDBBSFGRFMMPDJ8&marketplace=FLIPKART&srno=b_1_1&otracker=nmenu_sub_Home%20%26%20Furniture_0_Paintings&fm=productRecommendation%2Fsimilar&iid=en_JSrk%2BgBzV5pkXtkRlX5Otbjz0W0NqqjYrEGD9qrTv6bjmVMPcXoaGYz42arSUdD%2FpseQC5Yp8LtehAKGUd9%2B2Q%3D%3D&ppt=browse&ppn=browse&ssid=tay3wnoek00000001602515035289&page=3"]
+    # start_urls = [url]
+    def __init__(self, url='', *args, **kwargs):
+        super(reviewSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [url]
 
 
     def parse(self,response):
         
         root_url = "https://www.flipkart.com"
-        page = 2
+        
         print("HREF ATTRIBUTE:",Selector(response=response).xpath('//a/div/span/../../@href').get())
 
         next_page = Selector(response=response).xpath('//a/div/span/../../@href').get()
@@ -29,7 +21,7 @@ class reviewSpider(scrapy.Spider):
             # This is the product page and hence get the url to all reviews and go there
             print("NEXT PAGE IS:",root_url+next_page)
             yield scrapy.Request(
-                root_url+next_page+"&page=6",
+                root_url+next_page+"&page={}".format(self.page),
                 callback=self.parse
             )
         else:
@@ -52,7 +44,6 @@ class reviewSpider(scrapy.Spider):
             summary_row_xpath = '//html[1]/body/div/div/div[3]/div[1]/div[1]/div[2]/div[{}]'.format(review_summary_row_id)
 
             avg_summary_xpath = '/div/div[1]/div'
-            # review_summary_dict['avg_summary'] = Selector(response=response).xpath(summary_row_xpath+avg_summary_xpath).get()
             
             avg_rating_xpath = '/div[1]/div/div/text()'
             review_summary_dict['avg_rating'] = Selector(response=response).xpath(summary_row_xpath+avg_summary_xpath+avg_rating_xpath).get()
@@ -62,7 +53,6 @@ class reviewSpider(scrapy.Spider):
             
             total_reviews_xpath = '/div[3]/div/span/text()'
             review_summary_dict['total_reviews'] = Selector(response=response).xpath(summary_row_xpath+avg_summary_xpath+total_reviews_xpath).get()
-
             
             j = 5
             for i in range(1,6):
